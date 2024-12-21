ee5e415b344505765d4ad803375f6c4a3dbc576a
basha105 <brentbasha17@gmail.com>
2024-12-21 15:42:37 -0800

    Done testing prop types

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import RenderName from './RenderName.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RenderName />
   </StrictMode>,
 )
